ea8637d3109235ee0d55b0f74f040637691b6a04
Deveshb15 <deveshb583@gmail.com>
2021-07-14 17:55:34 +0530

    Updates

---
+++ precache-manifest.602ebabbc7d237f49f90efb472fe6220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ef5b6b2e1af6c98b22b6c4ff9efab7",
+    "url": "/ddrop/index.html"
+  },
+  {
+    "revision": "9b365f7c5d86dc34a670",
+    "url": "/ddrop/static/css/main.0337b3fe.chunk.css"
+  },
+  {
+    "revision": "4dcece01b5a77eccfdf2",
+    "url": "/ddrop/static/js/2.11198bf4.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/ddrop/static/js/2.11198bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b365f7c5d86dc34a670",
+    "url": "/ddrop/static/js/main.26645bab.chunk.js"
+  },
+  {
+    "revision": "bcfc59df8cce46019884",
+    "url": "/ddrop/static/js/runtime-main.3ef081ee.js"
+  },
+  {
+    "revision": "23ce584d6d7c43d64c70239db550f18f",
+    "url": "/ddrop/static/media/logo.23ce584d.png"
+  }
+]);
